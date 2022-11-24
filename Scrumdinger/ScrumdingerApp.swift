ae1b67f600c0333c4edecd0a206c559915c66d3b
Bernd Hengelein <bernd.hengelein@sivantos.com>
2022-11-24 09:03:35 +0100

    Section 3 of Scrumdinger tutorial
    Section 3 of Scrumdinger tutorial

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bernd Hengelein on 22.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
